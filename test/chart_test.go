121adc0ebe8841e5fcfdf530a23ac48e06f53b73
Maor Friedman <maor.friedman@nuvo-group.com>
2018-08-20 14:52:44 +0300

    add unit tests

---
+++ test/chart_test.go
@@ -0,0 +1,125 @@
+package test
+
+import (
+	"orca/pkg/utils"
+	"testing"
+)
+
+func TestGetReleasesDelta(t *testing.T) {
+	rel1 := utils.ReleaseSpec{ChartName: "chart1", ChartVersion: "1.0.0", ReleaseName: "dev-chart1"}
+	rel2 := utils.ReleaseSpec{ChartName: "chart2", ChartVersion: "2.0.0", ReleaseName: "dev-chart2"}
+
+	fromReleases := []utils.ReleaseSpec{rel1, rel2}
+	toReleases := []utils.ReleaseSpec{rel1}
+
+	releasesDelta := utils.GetReleasesDelta(fromReleases, toReleases)
+
+	if len(releasesDelta) != 1 {
+		t.Errorf("Expected: 1, Actual: " + (string)(len(releasesDelta)))
+	}
+
+	if !releasesDelta[0].Equals(rel2) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+}
+
+func TestChartsYamlToStruct(t *testing.T) {
+	file := "data/charts.yaml"
+	rel0 := utils.ReleaseSpec{ChartName: "cassandra", ChartVersion: "0.4.0", ReleaseName: "test-cassandra"}
+	rel1 := utils.ReleaseSpec{ChartName: "mariadb", ChartVersion: "0.5.4", ReleaseName: "test-mariadb"}
+	rel2 := utils.ReleaseSpec{ChartName: "kaa", ChartVersion: "0.1.7", ReleaseName: "test-kaa"}
+
+	releases := utils.ChartsYamlToStruct(file, "test")
+
+	if len(releases) != 3 {
+		t.Errorf("Expected: 3, Actual: " + (string)(len(releases)))
+	}
+	if !releases[0].Equals(rel0) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+	if !releases[1].Equals(rel1) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+	if !releases[2].Equals(rel2) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+}
+
+func TestOverrideReleases(t *testing.T) {
+	rel0 := utils.ReleaseSpec{ChartName: "cassandra", ChartVersion: "0.4.0", ReleaseName: "test-cassandra"}
+	rel1 := utils.ReleaseSpec{ChartName: "mariadb", ChartVersion: "0.5.4", ReleaseName: "test-mariadb"}
+	rel2 := utils.ReleaseSpec{ChartName: "kaa", ChartVersion: "0.1.7", ReleaseName: "test-kaa"}
+	rel2override := utils.ReleaseSpec{ChartName: "kaa", ChartVersion: "7.1.0", ReleaseName: "test-kaa"}
+
+	releases := []utils.ReleaseSpec{rel0, rel1, rel2}
+
+	overrideReleases := utils.OverrideReleases(releases, []string{"kaa=7.1.0"})
+
+	if !overrideReleases[2].Equals(rel2override) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+}
+
+func TestRemoveChartFromDependencies(t *testing.T) {
+	file := "data/charts.yaml"
+	releases := utils.ChartsYamlToStruct(file, "test")
+	releases = utils.RemoveChartFromDependencies(releases, "mariadb")
+
+	if len(releases[2].Dependencies) != 1 {
+		t.Errorf("Expected: 1, Actual: " + (string)(len(releases)))
+	}
+	if releases[2].Dependencies[0] != "cassandra" {
+		t.Errorf("Expected: cassandra, Actual: " + releases[2].Dependencies[0])
+	}
+}
+
+func TestGetChartIndex(t *testing.T) {
+	file := "data/charts.yaml"
+	releases := utils.ChartsYamlToStruct(file, "test")
+	index := utils.GetChartIndex(releases, "cassandra")
+
+	if index != 0 {
+		t.Errorf("Expected: 0, Actual: " + (string)(index))
+	}
+}
+
+func TestRemoveChartFromCharts(t *testing.T) {
+	rel1 := utils.ReleaseSpec{ChartName: "mariadb", ChartVersion: "0.5.4", ReleaseName: "test-mariadb"}
+	rel0 := utils.ReleaseSpec{ChartName: "kaa", ChartVersion: "0.1.7", ReleaseName: "test-kaa"}
+	file := "data/charts.yaml"
+	releases := utils.ChartsYamlToStruct(file, "test")
+	index := utils.GetChartIndex(releases, "cassandra")
+	releases = utils.RemoveChartFromCharts(releases, index)
+
+	if len(releases) != 2 {
+		t.Errorf("Expected: 2, Actual: " + (string)(len(releases)))
+	}
+	if !releases[0].Equals(rel0) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+	if !releases[1].Equals(rel1) {
+		t.Errorf("Expected: true, Actual: false")
+	}
+}
+
+func TestUpdateChartVersion(t *testing.T) {
+	newVersion := utils.UpdateChartVersion("data/", "1234")
+
+	if newVersion != "0.1.1-1234" {
+		t.Errorf("Expected: 0.1.1-1234, Actual: " + newVersion)
+	}
+}
+
+func TestEquals(t *testing.T) {
+	rel := utils.ReleaseSpec{ChartName: "mariadb", ChartVersion: "0.5.4", ReleaseName: "test-mariadb"}
+	equals := rel.Equals(rel)
+	if !equals {
+		t.Errorf("Expected: true, Actual: false")
+	}
+
+	relDiff := utils.ReleaseSpec{ChartName: "mariadba", ChartVersion: "0.5.4", ReleaseName: "test-mariadb"}
+	equals = rel.Equals(relDiff)
+	if equals {
+		t.Errorf("Expected: false, Actual: true")
+	}
+}
---
+++ test/git_test.go
@@ -0,0 +1,100 @@
+package test
+
+import (
+	"orca/pkg/utils"
+	"strconv"
+	"testing"
+)
+
+func TestGetBuildTypeByPathFilters(t *testing.T) {
+	var changedPaths = []string{}
+	var pathFilters = []string{"^src.*$=code", "^kubernetes.*$=chart"}
+	bt := utils.GetBuildTypeByPathFilters("default", changedPaths, pathFilters, false)
+	if bt != "default" {
+		t.Errorf("Expected: default, Actual: " + bt)
+	}
+
+	changedPaths = []string{"src/file1.go", "kubernetes/Chart.yaml"}
+	pathFilters = []string{"^src.*$=code", "^kubernetes.*$=chart"}
+	bt = utils.GetBuildTypeByPathFilters("default", changedPaths, pathFilters, false)
+	if bt != "default" {
+		t.Errorf("Expected: default, Actual: " + bt)
+	}
+
+	changedPaths = []string{"src/file1.go", "kubernetes/Chart.yaml"}
+	pathFilters = []string{"^src.*$=code", "^kubernetes.*$=chart"}
+	bt = utils.GetBuildTypeByPathFilters("default", changedPaths, pathFilters, true)
+	if bt != "code;chart" && bt != "chart;code" {
+		t.Errorf("Expected: code;chart, Actual: " + bt)
+	}
+
+	changedPaths = []string{"src/file1.go", "kubernetes/Chart.yaml", "other/file"}
+	pathFilters = []string{"^src.*$=code", "^kubernetes.*$=chart"}
+	bt = utils.GetBuildTypeByPathFilters("default", changedPaths, pathFilters, false)
+	if bt != "default" {
+		t.Errorf("Expected: default, Actual: " + bt)
+	}
+
+	changedPaths = []string{"src/file1.go", "kubernetes/Chart.yaml", "other/file"}
+	pathFilters = []string{"^src.*$=code", "^kubernetes.*$=chart"}
+	bt = utils.GetBuildTypeByPathFilters("default", changedPaths, pathFilters, true)
+	if bt != "default" {
+		t.Errorf("Expected: default, Actual: " + bt)
+	}
+}
+
+func TestIsMainlineOrReleaseRef(t *testing.T) {
+	isMainlineOrReleaseRef := utils.IsMainlineOrReleaseRef("master", "master", "^.*/rel-.*$")
+	if isMainlineOrReleaseRef != true {
+		t.Errorf("Expected: true, Actual: " + strconv.FormatBool(isMainlineOrReleaseRef))
+	}
+
+	isMainlineOrReleaseRef = utils.IsMainlineOrReleaseRef("fda/rel-1", "master", "^.*/rel-.*$")
+	if isMainlineOrReleaseRef != true {
+		t.Errorf("Expected: true, Actual: " + strconv.FormatBool(isMainlineOrReleaseRef))
+	}
+
+	isMainlineOrReleaseRef = utils.IsMainlineOrReleaseRef("develop", "master", "^.*/rel-.*$")
+	if isMainlineOrReleaseRef != false {
+		t.Errorf("Expected: false, Actual: " + strconv.FormatBool(isMainlineOrReleaseRef))
+	}
+}
+
+func TestIsCommitError(t *testing.T) {
+	isError := utils.IsCommitError("2f7444d674d79ea111483078e803cf3119c88e59", "E")
+	if isError != false {
+		t.Errorf("Expected: false, Actual: " + strconv.FormatBool(isError))
+	}
+
+	isError = utils.IsCommitError("E", "E")
+	if isError != true {
+		t.Errorf("Expected: false, Actual: " + strconv.FormatBool(isError))
+	}
+}
+
+func TestCountLinesPerPathFilter(t *testing.T) {
+	var changedPaths = []string{"src/file1.go", "kubernetes/Chart.yaml"}
+	var pathFilters = []string{"^src.*$=code", "^kubernetes.*$=chart"}
+	changedPathsPerFilter, changedPathsPerFilterCount := utils.CountLinesPerPathFilter(pathFilters, changedPaths)
+	if changedPathsPerFilterCount != 2 {
+		t.Errorf("Expected: 2, Actual: " + (string)(changedPathsPerFilterCount))
+	}
+	if changedPathsPerFilter["code"] != 1 {
+		t.Errorf("Expected: 1, Actual: " + (string)(changedPathsPerFilter["code"]))
+	}
+	if changedPathsPerFilter["chart"] != 1 {
+		t.Errorf("Expected: 2, Actual: " + (string)(changedPathsPerFilter["chart"]))
+	}
+
+	changedPaths = []string{"src/file1.go", "kubernetes/Chart.yaml", "other/file"}
+	changedPathsPerFilter, changedPathsPerFilterCount = utils.CountLinesPerPathFilter(pathFilters, changedPaths)
+	if changedPathsPerFilterCount != 2 {
+		t.Errorf("Expected: 2, Actual: " + (string)(changedPathsPerFilterCount))
+	}
+	if changedPathsPerFilter["code"] != 1 {
+		t.Errorf("Expected: 1, Actual: " + (string)(changedPathsPerFilter["code"]))
+	}
+	if changedPathsPerFilter["chart"] != 1 {
+		t.Errorf("Expected: 2, Actual: " + (string)(changedPathsPerFilter["chart"]))
+	}
+}
